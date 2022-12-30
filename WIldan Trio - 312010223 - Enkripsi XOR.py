22ce9751d05fe05591cac3022e3c51db36164a1f
WildanTrioM <121679664+WildanTrioM@users.noreply.github.com>
2022-12-30 21:41:12 +0700

    Add files via upload

---
+++ WIldan Trio - 312010223 - Enkripsi XOR.py
@@ -0,0 +1,25 @@
+# Wildan Trio Munawarudin
+# 312010223
+#Enkripsi XOR
+def encrypt(plaintext, key):
+  ciphertext = ""
+  for i in range(len(plaintext)):
+    ciphertext += chr(ord(plaintext[i]) ^ ord(key[i % len(key)]))
+  return ciphertext
+
+# Menggunakan XOR untuk deskripsi
+def decrypt(ciphertext, key):
+  plaintext = ""
+  for i in range(len(ciphertext)):
+    plaintext += chr(ord(ciphertext[i]) ^ ord(key[i % len(key)]))
+  return plaintext
+
+# Mencoba enkripsi dan deskripsi
+plaintext = "Halo, ini adalah teks asli"
+key = "sandi"
+
+ciphertext = encrypt(plaintext, key)
+print("Teks terenkripsi:", ciphertext)
+
+decrypted = decrypt(ciphertext, key)
+print("Teks yang didekripsi:", decrypted)
